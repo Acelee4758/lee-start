95f25d0eeec8fbdb22848741ba849e47b820d736
LYing <1191211691@qq.com>
2020-08-03 16:14:54 +0800

    lee

---
+++ 20200803/src/com/lee/test/ClassTest.java
@@ -0,0 +1,18 @@
+package com.lee.test;
+
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author AceLee
+ * 2020/8/3 10:32
+ */
+public class ClassTest {
+    @Test
+    public void work(){
+        String a = "HelloWorld";
+        System.out.println(a);
+    }
+}
